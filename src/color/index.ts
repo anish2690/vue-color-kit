c6be8fd132fda8808fb61f0faa59bc25a1be7f2f
Anish <anishgeorge2690@gmail.com>
2021-01-09 16:43:14 +0530

    fix: build errors

--- src/color/index.ts
+++ src/color/index.ts
@@ -1,5 +1,5 @@
 import { App } from 'vue'
-import VueColorKit from './index.vue'
+import VueColorKit from './ColorPicker.vue'
 
 VueColorKit.install = (Vue: App) => {
   Vue.component(VueColorKit.name, VueColorKit)
